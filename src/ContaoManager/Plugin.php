5cf8eb70d0dc7e81236050499e1269e4ab3e114a
Sven Meierhans <meierhans@men-at-work.de>
2018-11-06 09:56:34 +0100

    initial commit

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Contao - News most read bundle
+ *
+ * Created by MEN AT WORK Werbeagentur GmbH
+ *
+ * @copyright  MEN AT WORK Werbeagentur GmbH 2018
+ * @author     Sven Meierhans <meierhans@men-at-work.de>
+ */
+
+namespace MenAtWork\NewsMostReadBundle\ContaoManager;
+
+
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Contao\NewsBundle\ContaoNewsBundle;
+use MenAtWork\NewsMostReadBundle\NewsMostReadBundle;
+
+class Plugin implements BundlePluginInterface
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(NewsMostReadBundle::class)
+                ->setLoadAfter([ ContaoNewsBundle::class ])
+                ->setReplace([ 'faq' ]),
+        ];
+    }
+}
