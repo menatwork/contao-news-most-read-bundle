ea2970270c0edb6a76741bdc4a7470cdab54cd85
Sven Meierhans <meierhans@men-at-work.de>
2018-11-27 13:58:40 +0100

    * made read_count dca field writable

--- src/Resources/contao/dca/tl_news.php
+++ src/Resources/contao/dca/tl_news.php
@@ -26,6 +26,6 @@
 $GLOBALS['TL_DCA']['tl_news']['fields']['read_count'] = [
     'label'     => &$GLOBALS['TL_LANG']['tl_news']['read_count'],
     'inputType' => 'text',
-    'eval'      => [ 'rgxp' => 'digit', 'readonly' => true, 'tl_class' => 'w50' ],
+    'eval'      => [ 'rgxp' => 'digit', 'tl_class' => 'w50' ],
     'sql'       => "int(11) unsigned NOT NULL DEFAULT '0'",
 ];
