162d5da9ec02f348738919ab230e2dc06326702f
Daniel Jahnsmüller <mail@tastaturberuf.de>
2022-01-05 14:26:02 +0100

    Do not copy the read count if you duplicate the news

--- src/Resources/contao/dca/tl_news.php
+++ src/Resources/contao/dca/tl_news.php
@@ -9,6 +9,7 @@
  *
  * @author     Sven Meierhans <meierhans@men-at-work.de>
  * @author     Stefan Heimes <heimes@men-at-work.de>
+ * @author     Daniel Jahnsmüller <code@tastaturberuf.de>
  */
 
 /**
@@ -43,28 +44,28 @@
 $GLOBALS['TL_DCA']['tl_news']['fields']['read_count'] = [
     'label'     => &$GLOBALS['TL_LANG']['tl_news']['read_count'],
     'inputType' => 'text',
-    'eval'      => ['rgxp' => 'digit', 'tl_class' => 'w50'],
+    'eval'      => ['rgxp' => 'digit', 'doNotCopy' => true, 'tl_class' => 'w50'],
     'sql'       => "int(11) unsigned NOT NULL DEFAULT '0'",
 ];
 
 $GLOBALS['TL_DCA']['tl_news']['fields']['d_read_count_reset'] = [
     'label'     => &$GLOBALS['TL_LANG']['tl_news']['d_read_count_reset'],
     'inputType' => 'text',
-    'eval'      => ['rgxp' => 'digit', 'tl_class' => 'w50'],
+    'eval'      => ['rgxp' => 'digit',  'doNotCopy' => true, 'tl_class' => 'w50'],
     'sql'       => "int(11) unsigned NOT NULL DEFAULT '0'",
 ];
 
 $GLOBALS['TL_DCA']['tl_news']['fields']['dT_read_count'] = [
     'label'     => &$GLOBALS['TL_LANG']['tl_news']['dT_read_count'],
     'inputType' => 'text',
-    'eval'      => ['rgxp' => 'digit', 'tl_class' => 'w50'],
+    'eval'      => ['rgxp' => 'digit',  'doNotCopy' => true, 'tl_class' => 'w50'],
     'sql'       => "int(11) unsigned NOT NULL DEFAULT '0'",
 ];
 
 $basicSettings = [
     'label'     => &$GLOBALS['TL_LANG']['tl_news']['d_read_count'],
     'inputType' => 'text',
-    'eval'      => ['rgxp' => 'digit', 'tl_class' => 'w50'],
+    'eval'      => ['rgxp' => 'digit',  'doNotCopy' => true, 'tl_class' => 'w50'],
     'sql'       => "int unsigned NOT NULL DEFAULT '0'",
 ];
 
