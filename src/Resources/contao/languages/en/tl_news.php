17f9fafbfca39f6e0c6f9962927e20db60321f93
falkgeist <43405953+falkgeist@users.noreply.github.com>
2022-10-05 17:30:30 +0200

    Use improved english translations (news)

--- src/Resources/contao/languages/en/tl_news.php
+++ src/Resources/contao/languages/en/tl_news.php
@@ -9,6 +9,7 @@
  *
  * @author     Sven Meierhans <meierhans@men-at-work.de>
  * @author     Stefan Heimes <heimes@men-at-work.de>
+ * @author     Mathis Völkert <mvoelkert@kiwi.de>
  */
 
 $GLOBALS['TL_LANG']['tl_news']['read_count_legend']  = 'Read Counter';
@@ -26,3 +27,15 @@ $GLOBALS['TL_LANG']['tl_news']['dT_read_count']      = [
     'Here you can see the total count of the 7 days.'
 ];
 
+$GLOBALS['TL_LANG']['tl_news']['read_count_legend'] = 'Read counter';
+$GLOBALS['TL_LANG']['tl_news']['read_count'] = ['Views', 'This is the total count of views for this article.'];
+$GLOBALS['TL_LANG']['tl_news']['d_read_count_reset'] = ['Last reset', 'Shows the number of days since the counter was last reset.'];
+$GLOBALS['TL_LANG']['tl_news']['dT_read_count'] = ['7-day count', 'This is the total count of views for the last 7 days.'];
+$GLOBALS['TL_LANG']['tl_news']['d0_read_count'] = ['Views - today', 'This is the total count of views for today.'];
+$GLOBALS['TL_LANG']['tl_news']['d1_read_count'] = ['Views - yesterday', 'This is the total count of views for yesterday.'];
+$GLOBALS['TL_LANG']['tl_news']['d2_read_count'] = ['Views - 2 days ago', 'This is the total count of views, 2 days ago.'];
+$GLOBALS['TL_LANG']['tl_news']['d3_read_count'] = ['Views - 3 days ago', 'This is the total count of views, 3 days ago.'];
+$GLOBALS['TL_LANG']['tl_news']['d4_read_count'] = ['Views - 4 days ago', 'This is the total count of views, 4 days ago.'];
+$GLOBALS['TL_LANG']['tl_news']['d5_read_count'] = ['Views - 5 days ago', 'This is the total count of views, 5 days ago.'];
+$GLOBALS['TL_LANG']['tl_news']['d6_read_count'] = ['Views - 6 days ago', 'This is the total count of views, 6 days ago.'];
+$GLOBALS['TL_LANG']['tl_news']['d7_read_count'] = ['Views - 7 days ago', 'This is the total count of views, 7 days ago.'];
