76a8ae7d155d553779bc54a2ea015c49ad4ca5ea
falkgeist <43405953+falkgeist@users.noreply.github.com>
2022-10-05 17:04:46 +0200

    Use improved labels

--- src/Resources/contao/languages/de/tl_news.php
+++ src/Resources/contao/languages/de/tl_news.php
@@ -9,20 +9,18 @@
  *
  * @author     Sven Meierhans <meierhans@men-at-work.de>
  * @author     Stefan Heimes <heimes@men-at-work.de>
+ * @author     Mathis Völkert <mvoelkert@kiwi.de>
  */
 
-$GLOBALS['TL_LANG']['tl_news']['read_count_legend']  = 'Read Counter';
-$GLOBALS['TL_LANG']['tl_news']['read_count']         = ['Count', 'Anzahl wie oft die Nachricht gelesen wurde.'];
-$GLOBALS['TL_LANG']['tl_news']['d_read_count']       = [
-    'Täglicher Count',
-    'Anzahl wie oft die Nachricht gelesen wurde.'
-];
-$GLOBALS['TL_LANG']['tl_news']['d_read_count_reset'] = [
-    'Tag der letzten zurück setzung',
-    'Gibt an an welchen Tag der Counter das letzte mal zurück gesetzt wurde.'
-];
-$GLOBALS['TL_LANG']['tl_news']['dT_read_count']      = [
-    '7 Tage count',
-    'Hier sehen Sie den gesamt Wert der 7 Tage.'
-];
-
+$GLOBALS['TL_LANG']['tl_news']['read_count_legend'] = '"Gelesen"-Zähler';
+$GLOBALS['TL_LANG']['tl_news']['read_count'] = ['Anzahl Aufrufe', 'Dies ist die gesamte Anzahl der Aufrufe dieser Nachricht.'];
+$GLOBALS['TL_LANG']['tl_news']['d_read_count_reset'] = ['Tag der letzten Zurücksetzung', 'Gibt an, vor wie vielen Tagen der Zähler zuletzt zurückgesetzt wurde.'];
+$GLOBALS['TL_LANG']['tl_news']['dT_read_count'] = ['7-Tage-Zähler', 'Dies ist die Anzahl der Aufrufe der letzten 7 Tage.'];
+$GLOBALS['TL_LANG']['tl_news']['d0_read_count'] = ['Zähler - heute', 'Dies ist die Anzahl der Aufrufe vom aktuellen Tag.'];
+$GLOBALS['TL_LANG']['tl_news']['d1_read_count'] = ['Zähler - gestern', 'Dies ist die Anzahl der Aufrufe vom vorigen Tag.'];
+$GLOBALS['TL_LANG']['tl_news']['d2_read_count'] = ['Zähler - vorgestern', 'Dies ist die Anzahl der Aufrufe vor 2 Tagen.'];
+$GLOBALS['TL_LANG']['tl_news']['d3_read_count'] = ['Zähler - vor 3 Tagen', 'Dies ist die Anzahl der Aufrufe vor 3 Tagen.'];
+$GLOBALS['TL_LANG']['tl_news']['d4_read_count'] = ['Zähler - vor 4 Tagen', 'Dies ist die Anzahl der Aufrufe vor 4 Tagen.'];
+$GLOBALS['TL_LANG']['tl_news']['d5_read_count'] = ['Zähler - vor 5 Tagen', 'Dies ist die Anzahl der Aufrufe vor 5 Tagen.'];
+$GLOBALS['TL_LANG']['tl_news']['d6_read_count'] = ['Zähler - vor 6 Tagen', 'Dies ist die Anzahl der Aufrufe vor 6 Tagen.'];
+$GLOBALS['TL_LANG']['tl_news']['d7_read_count'] = ['Zähler - vor 7 Tagen', 'Dies ist die Anzahl der Aufrufe vor 7 Tagen.'];
